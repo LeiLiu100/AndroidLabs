741109954307ebc99590b19863e40a89f13db743
Lei Liu <liu00439@algonquinlive.com>
2019-02-05 12:12:01 -0500

    lab3 commit v1

---
+++ app/src/main/java/com/example/myapplication/Main-Activity.java
@@ -0,0 +1,84 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    EditText userEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {//read
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab4);
+
+        //this.getDelegate().
+        userEmail = (EditText) findViewById(R.id.email_Text);
+        sharedPreferences = getSharedPreferences("FileUserName", Context.MODE_PRIVATE);
+        String savedString = sharedPreferences.getString("UserEmail", "Dv ");
+        userEmail.setText(savedString);
+
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener( c -> {
+            Intent profilePage = new Intent(MainActivity.this, ProfileActivity.class);
+            //Give directions to go from this page, to ProfileActivity
+            EditText et = (EditText)findViewById(R.id.email_Text);
+            profilePage.putExtra("ItemOne", "Some text");
+            profilePage.putExtra("ItemTwo", 500);
+            profilePage.putExtra("typed", et.getText().toString());
+            //Now make the transition:
+            startActivityForResult( profilePage, 345);
+        });
+    }
+    @Override
+    protected void onPause() {// save
+        super.onPause();
+        //get an editor object
+        //sharedPre = getSharedPreferences("UserFile", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //save what was typed under the name "ReserveName"
+        String userTypedEmail = userEmail.getText().toString();
+        editor.putString("UserEmail", userTypedEmail);
+
+        //write it to disk:
+        editor.commit();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        int i = 0;
+        i++;
+        //if request code is 345, then we are coming back from ThirdActivity, as written on line 38
+        if(requestCode == 345)
+        {
+            // resultCode will only be 60 if the user clicks on the back button on page 3 (ThirdActivity.java line 39)
+            if(resultCode == 60)
+            {
+                EditText et = findViewById(R.id.email_Text);
+                String fromPageThree = data.getStringExtra("NextPageTyped");
+                et.setText(fromPageThree);
+                Log.i("Back", "Message");
+            }
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.graphics.Bitmap;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+public class ProfileActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    ImageButton mImageButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab4);
+
+        // get the intent that got us here
+        //Intent fromPrevious = getIntent();
+        //String previousTyped = fromPrevious.getStringExtra("typed");
+        //String someText = fromPrevious.getStringExtra("ItemOne");
+        //int extraInt = fromPrevious.getIntExtra("ItemTwo", 0);
+        //Put the string that was sent from FirstActivity into the edit text:
+        //EditText enterText = (EditText)findViewById(R.id.email_Text);
+        //enterText.setText(previousTyped);
+
+        mImageButton = (ImageButton)findViewById(R.id.picButton);
+        mImageButton.setOnClickListener(new View.OnClickListener();
+        mImageButton.setOnClickListener(v -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+        Button chatBt = (Button) findViewById(R.id.buttonGotoChat);
+        chatBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, ChatRoomActivity.class);
+                startActivity(intent);
+            }
+        });
+        Log.e(ACTIVITY_NAME, "In function:" + "onCreate");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(ACTIVITY_NAME, "In function:" + "onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+
+    @Override
+    protected void onPause() {// save
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function:" + "onPause");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function:" + "onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function:" + "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStart");
+    }
+}
